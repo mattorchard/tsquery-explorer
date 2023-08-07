d114998e88193cc9936ca2cf7c3a41cc6d29284c
Matt Orchard <morch028@uottawa.ca>
2023-08-06 22:35:01 -0400

    Add monaco

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import monacoEditorPlugin from "vite-plugin-monaco-editor";
 
 export default defineConfig({
-  plugins: [solid()],
-})
+  plugins: [
+    solid(),
+    // @ts-ignore
+    monacoEditorPlugin.default({}),
+  ],
+});
